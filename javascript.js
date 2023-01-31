ddfe5ccf8baeeaea1c6e0fc112097a123a396a29
EXPIcaruz <88259135+EXPIcaruz@users.noreply.github.com>
2023-01-31 04:38:25 -0500

    created 1st function

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//Your game is going to play against the computer, so begin with a
+//function called getComputerChoice that will randomly return either
+//‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make
+//the computer’s play. Tip: use the console to make sure this is returning
+// the expected output before moving to the next step!
+
+// set varibles for choice 
+
+
+const choice = [ 'rock', 'paper', 'scissors' ];
+const compPlay = Math.floor(Math.random() * choice.length);
+
+console.log(choice[compPlay]);
+
+
+
+
+
+
+
+// set function for computer to randomy choose
+// set function for single round where 
+// set loop for multiple rounds 
+// set user input system 
